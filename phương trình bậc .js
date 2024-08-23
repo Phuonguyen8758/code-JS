6008004c49b044e73e7f07f6b73acdeb196495eb
Phuonguyen8758 <dominhnguyen279@gmail.com>
2024-08-23 12:04:12 +0700

    Create phương trình bậc 1 .js

---
+++ phương trình bậc .js
@@ -0,0 +1,19 @@
+<html>
+  <title>Phương Trình Bậc 1</title>
+<body>
+      <script type="text/JavaScript">
+      var a = prompt("a = ")
+      var b = prompt ("b = ")
+      if (a == 0 ){
+           document.write ("Vo so nghiem");
+      if (b!= 0) 
+      {
+           document.write("vo nghiem");
+      }
+    }
+    else 
+    var x = -b/a 
+document.write("x =" +x)
+    </script>
+</body>
+</html>
